1ea5320c237d134a04b6266157de9e8ac44897c6
Maria Angelica <mariancademico@gmail.com>
2022-08-22 14:07:18 -0400

    em aula

--- bubble-sort/bubble-main.c
+++ bubble-sort/bubble-main.c
@@ -7,53 +7,24 @@ void cont_tempo (int *vector, int size);
 
 int main (void){
 
-    int tam_vetor1 = 500;
-    int *vetor1;
+    int tam_vetor = 500;
+    int *vetorPont;
 
     srand(time(NULL)); //funcao de contagem de tempo
 
-    vetor1 = (int *) malloc(tam_vetor1 *sizeof(int)); //alocando memoria para o ponteiro vetor
-    if(vetor1 == NULL){
+    vetorPont = (int *) malloc(tam_vetor *sizeof(int)); //alocando memoria para o ponteiro vetor
+    bubble(vetorPont, tam_vetor);
+    if(vetorPont == NULL){
         printf("ERRO AO ALOCAR VETOR \n");
         exit(1);
     }
-    cont_tempo(vetor1, tam_vetor1);
+    cont_tempo(vetorPont, tam_vetor);
 
-    int tam_vetor2 = 1000;
-    int *vetor2;
-    vetor2 = (int *) malloc(tam_vetor2 *sizeof(int)); //alocando memoria para o ponteiro vetor
-        if(vetor2 == NULL){
-        printf("ERRO AO ALOCAR VETOR \n");
-        exit(1);
-    }
-    cont_tempo(vetor2, tam_vetor2); 
-
-    int tam_vetor2 = 10000;
-    int *vetor2;
-    vetor2 = (int *) malloc(tam_vetor2 *sizeof(int)); //alocando memoria para o ponteiro vetor
-        if(vetor2 == NULL){
-        printf("ERRO AO ALOCAR VETOR \n");
-        exit(1);
-    }
-    cont_tempo(vetor2, tam_vetor2);   
-
-    int tam_vetor2 = 500000;
-    int *vetor2;
-    vetor2 = (int *) malloc(tam_vetor2 *sizeof(int)); //alocando memoria para o ponteiro vetor
-        if(vetor2 == NULL){
-        printf("ERRO AO ALOCAR VETOR \n");
-        exit(1);
-    }
-    cont_tempo(vetor2, tam_vetor2); 
-
-    int tam_vetor2 = 100000000;
-    int *vetor2;
-    vetor2 = (int *) malloc(tam_vetor2 *sizeof(int)); //alocando memoria para o ponteiro vetor
-        if(vetor2 == NULL){
-        printf("ERRO AO ALOCAR VETOR \n");
-        exit(1);
-    }
-    cont_tempo(vetor2, tam_vetor2); 
+    tam_vetor = 1000;
+    
+    vetorPont = (int *) realloc(vetorPont, tam_vetor);
+    bubble(vetorPont, tam_vetor);
+    cont_tempo(vetorPont, tam_vetor);
 
     return 0;
-}
+}   
